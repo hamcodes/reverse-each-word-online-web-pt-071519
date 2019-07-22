e6350f6092ede52ddd7c8e726663e424c786bdb7
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-22 13:56:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence)
-  sentence = []
-  sentence.each do |sentence|
-  sentence.reverse
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
   end
+  test_array.join(" ")
 end
